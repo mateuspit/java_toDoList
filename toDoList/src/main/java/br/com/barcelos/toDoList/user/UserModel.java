a9a98ad51aea86bf6a5a1590e536d8bfa0db39a7
Mateus Barcelos <mateuspit@hotmail.com>
2023-10-14 22:07:07 -0300

    build: create UserModel class

--- toDoList/src/main/java/br/com/barcelos/toDoList/user/UserModel.java
+++ toDoList/src/main/java/br/com/barcelos/toDoList/user/UserModel.java
@@ -5,4 +5,27 @@ public class UserModel {
     private String name;
     private String password;
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
 }
