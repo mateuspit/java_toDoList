4cce5d01598a296c6222536b45ec3662f2c3f152
Mateus Barcelos <mateuspit@hotmail.com>
2023-10-14 21:22:56 -0300

    fix: making a maven project

--- toDoList/src/main/java/br/com/barcelos/toDoList/ToDoListApplication.java
+++ toDoList/src/main/java/br/com/barcelos/toDoList/ToDoListApplication.java
@@ -2,12 +2,13 @@ package br.com.barcelos.toDoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ToDoListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApplication.class, args);
+    }
 
 }
---
+++ toDoList/src/main/java/br/com/barcelos/toDoList/user/UserModel.java
@@ -0,0 +1,8 @@
+package br.com.barcelos.toDoList.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+}
