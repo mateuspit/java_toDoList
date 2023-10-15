1601a71d9338cddb7bf7a5802b3b237025512e7a
Mateus Barcelos <mateuspit@hotmail.com>
2023-10-15 17:38:00 -0300

    build: create users table in DB

--- toDoList/src/main/java/br/com/barcelos/toDoList/user/UserModel.java
+++ toDoList/src/main/java/br/com/barcelos/toDoList/user/UserModel.java
@@ -1,10 +1,34 @@
 package br.com.barcelos.toDoList.user;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 //Podemos usar lib como o lombok para criar getters e setters automaticamente e mais limpo
+@Entity(name = "tb_users")
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUDI")
+    private String UUID;
+    @Column(name = "username_column") // Apenas mostrando a possibilidade de mudar o nome da coluna
     private String username;
     private String name;
     private String password;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+
+    public String getUUID() {
+        return this.UUID;
+    }
 
     public void setUsername(String username) {
         this.username = username;
