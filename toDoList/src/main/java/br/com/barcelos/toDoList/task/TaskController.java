7c93d9f9392f0ca002203f308ffc7535a7378efa
Mateus Barcelos <mateuspit@hotmail.com>
2023-10-16 16:28:45 -0300

    feat: update task keys objects

--- toDoList/src/main/java/br/com/barcelos/toDoList/task/TaskController.java
+++ toDoList/src/main/java/br/com/barcelos/toDoList/task/TaskController.java
@@ -8,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.barcelos.toDoList.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -46,5 +49,14 @@ public class TaskController {
         var userId = request.getAttribute("userId");
         var userTasks = this.taskRepository.findByUserId((UUID) userId);
         return userTasks;
+    }
+
+    @PutMapping("/{id}")
+    public TaskModel updateTask(@RequestBody TaskModel taskModel, @PathVariable UUID id) {
+        var task = this.taskRepository.findById(id).orElse(null);
+
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
 }
