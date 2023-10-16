5e3b994f75756e927718ddb9f1b35ecb1417b71f
Mateus Barcelos <mateuspit@hotmail.com>
2023-10-16 16:04:09 -0300

    feat: get all user tasks using auth by user id

--- toDoList/src/main/java/br/com/barcelos/toDoList/task/ITaskRepository.java
+++ toDoList/src/main/java/br/com/barcelos/toDoList/task/ITaskRepository.java
@@ -3,7 +3,8 @@ package br.com.barcelos.toDoList.task;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
-
+    List<TaskModel> findByUserId(UUID userId);
 }
--- toDoList/src/main/java/br/com/barcelos/toDoList/task/TaskController.java
+++ toDoList/src/main/java/br/com/barcelos/toDoList/task/TaskController.java
@@ -1,11 +1,13 @@
 package br.com.barcelos.toDoList.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,6 @@ public class TaskController {
 
     @PostMapping("/create")
     public ResponseEntity createTask(@RequestBody TaskModel taskModel, HttpServletRequest request) {
-
         var currentDate = LocalDateTime.now();
         if (currentDate.isAfter(taskModel.getEndAt()) || currentDate.isAfter(taskModel.getStartAt())) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -38,5 +39,12 @@ public class TaskController {
 
         var newTask = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> getAllTasksByUserId(HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        var userTasks = this.taskRepository.findByUserId((UUID) userId);
+        return userTasks;
     }
 }
